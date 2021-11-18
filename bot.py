dfde234147d0ac4f963e7aa26d3cdf55723535e2
mikh-maksi <newlevelcenter@gmail.com>
2021-11-18 10:44:47 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,15 @@
+from telegram.ext import Updater, MessageHandler, Filters
+
+def echo(update, context):
+    string_in = update.message.text
+    string_out = string_in
+    update.message.reply_text(string_out)
+
+updater = Updater("2061557281:AAHCga1lcSwI182tV4K29nWN3IEp1P8dTHo")
+
+dispatcher = updater.dispatcher
+
+dispatcher.add_handler(MessageHandler(Filters.all, echo))
+
+updater.start_polling()
+updater.idle()
