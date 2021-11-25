5daffb65a4d53d2948701b4b4f6a21935826d67c
Максимов Михаил Сергеевич <Maksymov@itstep.kharkov.ua>
2021-11-25 12:44:52 +0200

    start

--- timedbot.py
+++ timedbot.py
@@ -1,39 +1,82 @@
-import schedule
-import time
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Hi! Use /set <seconds> to set a timer')
+
+def alarm(context: CallbackContext) -> None:
+    job = context.job
+    context.bot.send_message(job.context, text='Beep!')
+
+def info(context: CallbackContext) -> None:
+    job = context.job
+    context.bot.send_message(job.context, text='Info')
+
+def remove_job_if_exists(name: str, context: CallbackContext) -> bool:
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
 
 
-from telegram.ext import Updater, MessageHandler, Filters
+def set_timer(update: Update, context: CallbackContext) -> None:
+    chat_id = update.message.chat_id
+    try:
+        # args[0] should contain the time for the timer in seconds
+        due = int(context.args[0])
+        if due < 0:
+            update.message.reply_text('Sorry we can not go back to future!')
+            return
 
-def job():
-    print("I'm working...")
+        job_removed = remove_job_if_exists(str(chat_id), context)
+        context.job_queue.run_once(alarm, due, context=chat_id, name=str(chat_id))
 
-def job_m():
-    print("Minute...")
-    chat = update.effective_chat
-    context.bot.send_message(chat_id=chat.id, text=str(total_dict))
+        text = 'Timer successfully set!'
+        if job_removed:
+            text += ' Old one was removed.'
+        update.message.reply_text(text)
 
-# schedule.every(10).minutes.do(job)
-# schedule.every().hour.do(job)
-# schedule.every().day.at("23:00").do(job)
-# schedule.every().monday.do(job)
-# schedule.every().wednesday.at("13:15").do(job)
-schedule.every().minute.at(":17").do(job_m)
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /set <seconds>')
 
 
-def echo(update, context):
-    string_in = update.message.text
-    string_out = string_in
-    update.message.reply_text(string_out)
+def unset(update: Update, context: CallbackContext) -> None:
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    text = 'Timer successfully cancelled!' if job_removed else 'You have no active timer.'
+    update.message.reply_text(text)
 
-updater = Updater("2013484013:AAGNF0KtAimfPfyLR2yPZs-JYsJNv7sFRwA")
+def st(update: Update, context: CallbackContext) -> None:
+    chat_id = update.message.chat_id
+    context.job_queue.run_repeating(info, 1, context=chat_id, name=str(chat_id))
 
-dispatcher = updater.dispatcher
+def stp(update: Update, context: CallbackContext) -> None:
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    text = 'Timer successfully cancelled!' if job_removed else 'You have no active timer.'
+    update.message.reply_text(text)
 
-dispatcher.add_handler(MessageHandler(Filters.all, echo))
+def main() -> None:
+    """Run bot."""
+    updater = Updater("2013484013:AAGNF0KtAimfPfyLR2yPZs-JYsJNv7sFRwA")
 
-updater.start_polling()
-updater.idle()
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", start))
+    dispatcher.add_handler(CommandHandler("set", set_timer))
+    dispatcher.add_handler(CommandHandler("unset", unset))
+    dispatcher.add_handler(CommandHandler("st", st))
+    dispatcher.add_handler(CommandHandler("stp", stp))
+    
+
+    updater.start_polling()
+
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
